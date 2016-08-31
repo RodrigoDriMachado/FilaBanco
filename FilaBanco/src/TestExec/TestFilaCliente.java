6e5fad51660140023de3d9e904def99bd078421c
RodrigoDriMachado <rodrigoodm46@gmail.com>
2016-08-31 14:54:57 -0300

    Ajuste métodos de teste
    Adicionado mais dois casos de teste e ajustado o método que teste o sizeNormal

--- FilaBanco/src/TestExec/TestFilaCliente.java
+++ FilaBanco/src/TestExec/TestFilaCliente.java
@@ -28,15 +28,7 @@ public class TestFilaCliente {
 		fila_clnt.addCliente(clnt5);
 	}
 	
-	@Test
-	public void testeSizeNormal(){
-		fila_clnt.cleanFilaNormal();
-		fila_clnt.addCliente(clnt1);
-		fila_clnt.addCliente(clnt2);
-		fila_clnt.addCliente(clnt3);
-		assertEquals(2, fila_clnt.sizeNormal());
-    } 
-	
+
 	@Test
 	public void testSizeNormal(){
 		fila_clnt.cleanFilaNormal();
@@ -47,9 +39,30 @@ public class TestFilaCliente {
 		fila_clnt.addCliente(clnt5);
 		fila_clnt.removeNormal();
 		fila_clnt.removeNormal();
-		fila_clnt.removeNormal();
+		fila_clnt.removePrioritario();
 		assertEquals(0, fila_clnt.sizeNormal());
     } 
+
+	@Test
+	public void testeAtendimentoPrioritario() {
+		fila_clnt.cleanFilaPrioritaria();
+		fila_clnt.addCliente(clnt3);
+		fila_clnt.addCliente(clnt4);
+		fila_clnt.addCliente(clnt5);
+		fila_clnt.removePrioritario();
+		fila_clnt.removePrioritario();
+		assertEquals(1,fila_clnt.sizePrioritaria());
+	}
 	
+	@Test
+	public void testAtendimentoNormal() {
+		fila_clnt.cleanFilaPrioritaria();
+		fila_clnt.addCliente(clnt1);
+		fila_clnt.addCliente(clnt2);
+		fila_clnt.removeNormal();
+		fila_clnt.removeNormal();
+		Cliente clnt6 = new Cliente(26,"Eduardo");
+		assertEquals(1,fila_clnt.sizePrioritaria());
+	}
 	
 }
